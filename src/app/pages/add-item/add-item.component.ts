77351b270824a3124e15ea98b9d79069a188cc21
Gehan1226 <gehansithija2023@gmail.com>
2024-07-26 16:16:25 +0530

    add item method

--- src/app/pages/add-item/add-item.component.ts
+++ src/app/pages/add-item/add-item.component.ts
@@ -1,11 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-add-item',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './add-item.component.html',
   styleUrl: './add-item.component.css'
 })
@@ -19,10 +21,35 @@ export class AddItemComponent {
     rentalPerDay: undefined,
     finePerDay: undefined,
     qtyOnHand: undefined,
-    isAvailable: undefined
   }
 
   addItem() {
-    throw new Error('Method not implemented.');
+    if (this.item.name && this.item.rentalPerDay && this.item.finePerDay && this.item.qtyOnHand) {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, save it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+          this.http.post("http://localhost:8080/items/save", this.item, { responseType: 'text' })
+            .subscribe(
+              data => {
+                console.log(data);
+              }
+            );
+
+          Swal.fire({
+            title: "Saved!",
+            text: "Customer has been Saved.",
+            icon: "success"
+          });
+        }
+      });
     }
+  }
 }
