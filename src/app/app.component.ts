8ac9da6c9d948972b7d4cbbb964d33a240d8d39c
Gehan1226 <gehansithija2023@gmail.com>
2024-07-26 16:38:17 +0530

    add rental method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AddCustomerComponent } from "./pages/add-customer/add-customer.component";
 import { ViewAllCustomersComponent } from "./pages/view-all-customers/view-all-customers.component";
 import { AddItemComponent } from "./pages/add-item/add-item.component";
+import { AddRentalComponent } from "./pages/add-rental/add-rental.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddCustomerComponent, ViewAllCustomersComponent, AddItemComponent],
+  imports: [RouterOutlet, AddCustomerComponent, ViewAllCustomersComponent, AddItemComponent, AddRentalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/add-rental/add-rental.component.ts
+++ src/app/pages/add-rental/add-rental.component.ts
@@ -1,12 +1,35 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-rental',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './add-rental.component.html',
   styleUrl: './add-rental.component.css'
 })
 export class AddRentalComponent {
 
+  constructor(private http: HttpClient) { }
+
+
+  public rental = {
+    id: undefined,
+    rentalDate: undefined,
+    returndate: undefined,
+    dueDate: undefined,
+    totalCost: undefined,
+    itemIdList: undefined,
+    customerId: undefined
+  }
+
+  addRental() {
+    this.http.post("http://localhost:8080/rentals/save", null, { responseType: 'text' })
+      .subscribe(
+        data => {
+          console.log(data);
+        }
+      );
+  }
 }
